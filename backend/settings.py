2d1cba635cb568510abf0e40ff241938988520c1
Mark Deng <90063806+MarkDeng86@users.noreply.github.com>
2024-02-12 12:42:13 -0500

    update

--- backend/settings.py
+++ backend/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ateam',
         'USER': 'root',
-        'PASSWORD': 'abc123',
+        'PASSWORD': '',
         'HOST':'localhost',
         'PORT':'3306',
     }
