8ba8111280f019d02b3c56fabe14b0e0df013dc7
dahongj <jiagndahong2001@gmail.com>
2024-03-04 13:12:59 -0500

    migrate

--- backend/settings.py
+++ backend/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ateam',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': '9768',
         'HOST':'localhost',
         'PORT':'3306',
     }
